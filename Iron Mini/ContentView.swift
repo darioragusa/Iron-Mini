3bd45b4af2eca928ce8b4c3390071d0ba05d1638
Dario Ragusa <46404000+Miaosi001@users.noreply.github.com>
2022-09-18 15:56:30 +0200

    Initial Commit

---
+++ Iron Mini/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Iron Mini
+//
+//  Created by Dario Ragusa on 18/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Iron Mini/Iron_MiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  Iron_MiniApp.swift
+//  Iron Mini
+//
+//  Created by Dario Ragusa on 18/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Iron_MiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
